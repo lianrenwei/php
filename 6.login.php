a29b915e75e7be19c5118ed06e8f840faddfbdc2
連人葳 <132965916+lianrenwei@users.noreply.github.com>
2025-04-13 03:17:25 +0800

    6.login.php

---
+++ 6.login.php
@@ -0,0 +1,18 @@
+<?php
+   #mysqli_connect() 建立資料庫連結
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+   #mysqli_query() 從資料庫查詢資料
+   $result=mysqli_query($conn, "select * from user");
+   #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
+   $login=FALSE; 
+   while ($row=mysqli_fetch_array($result)) {
+     if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) #對比帳號密碼是否匹配  
+     {
+       $login=TRUE; #匹配表示登入成功
+     }
+   } 
+   if ($login==TRUE)  #匹配成功
+     echo "登入成功";  #顯示登入成功
+  else
+     echo "帳號/密碼 錯誤"; #顯示帳號/密碼 錯誤
+?>
