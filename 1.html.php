80d9a26ddeb0ee16a384b0c2f5b92ea9fcb08577
連人葳 <132965916+lianrenwei@users.noreply.github.com>
2025-04-13 03:14:21 +0800

    1.html.php

---
+++ 1.html.php
@@ -0,0 +1,10 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> #echo列印 Hello world!
+        <h1>html</h1>                 #最大的重要標題 html
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+    </body>
+</html>
